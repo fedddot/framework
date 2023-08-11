b97e756b5d02858f0104a8e615540cd4b0e0ae59
idan.rom <idanrommm@gmail.com>
2023-08-12 02:19:20 +0300

    updated

--- communication/include/icommunication_manager.hpp
+++ communication/include/icommunication_manager.hpp
@@ -8,9 +8,7 @@ namespace communication {
 	template <class Trawdata, class Tdata>
 	class ICommunicationManager: public IReceiver<Trawdata, Tdata>, public ISender<Tdata> {
 	public:
-		virtual void start() = 0;
-		virtual void stop() = 0;
-		virtual bool isRunning() const = 0;
+
 	}; // ICommunicationManager
 } // namespace communication
 #endif // __ICOMMUNICATION_MANAGER_HPP__

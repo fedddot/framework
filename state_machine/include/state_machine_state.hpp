cd1a5b45dcd6227008d7256f0364effd3d9ef637
idan.rom <idanrommm@gmail.com>
2023-12-01 14:13:58 +0200

    class renamed

--- state_machine/include/state_machine_state.hpp
+++ state_machine/include/state_machine_state.hpp
@@ -6,15 +6,15 @@
 
 namespace state_machine {
 	template <class Tstate_id, class Tcontext_data, class Tevent>
-	class StateBase {
+	class StateMachineState {
 	public:
 		typedef StateMachineContext<Tstate_id, Tcontext_data> Context;
 		typedef std::function<void(Context *, const Tevent&)> StateHandler;
 
-		StateBase(const Tstate_id& id, const StateHandler& state_handler);
-		StateBase(const StateBase& other) = default;
-		StateBase& operator=(const StateBase& other) = default;
-		virtual ~StateBase() noexcept = default;
+		StateMachineState(const Tstate_id& id, const StateHandler& state_handler);
+		StateMachineState(const StateMachineState& other) = default;
+		StateMachineState& operator=(const StateMachineState& other) = default;
+		virtual ~StateMachineState() noexcept = default;
 
 		inline Tstate_id id() const;
 		inline void handle_event(Context *context_ptr, const Tevent& event) const;
@@ -24,17 +24,17 @@ namespace state_machine {
 	};
 
 	template <class Tstate_id, class Tcontext, class Tevent>
-	StateBase<Tstate_id, Tcontext, Tevent>::StateBase(const Tstate_id& id, const StateHandler& state_handler): m_id(id), m_handler(state_handler) {
+	StateMachineState<Tstate_id, Tcontext, Tevent>::StateMachineState(const Tstate_id& id, const StateHandler& state_handler): m_id(id), m_handler(state_handler) {
 
 	}
 
 	template <class Tstate_id, class Tcontext, class Tevent>
-	inline Tstate_id StateBase<Tstate_id, Tcontext, Tevent>::id() const {
+	inline Tstate_id StateMachineState<Tstate_id, Tcontext, Tevent>::id() const {
 		return m_id;
 	}
 
 	template <class Tstate_id, class Tcontext, class Tevent>
-	inline void StateBase<Tstate_id, Tcontext, Tevent>::handle_event(Context *context_ptr, const Tevent& event) const {
+	inline void StateMachineState<Tstate_id, Tcontext, Tevent>::handle_event(Context *context_ptr, const Tevent& event) const {
 		m_handler(context_ptr, event);
 	}
 

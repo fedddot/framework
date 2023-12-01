6a250d61c548bd42db39cee3f02d347209dab462
idan.rom <idanrommm@gmail.com>
2023-12-01 14:13:22 +0200

    class renamed

--- state_machine/include/state_machine_event.hpp
+++ state_machine/include/state_machine_event.hpp
@@ -3,12 +3,12 @@
 
 namespace state_machine {
 	template <class Tevent_id, class Tevent_data>
-	class EventBase {
+	class StateMachineEvent {
 	public:
-		EventBase(const Tevent_id& id, const Tevent_data& data);
-		EventBase(const EventBase& other) = default;
-		EventBase& operator=(const EventBase& other) = default;
-		virtual ~EventBase() noexcept = default;
+		StateMachineEvent(const Tevent_id& id, const Tevent_data& data);
+		StateMachineEvent(const StateMachineEvent& other) = default;
+		StateMachineEvent& operator=(const StateMachineEvent& other) = default;
+		virtual ~StateMachineEvent() noexcept = default;
 		inline Tevent_id id() const;
 		inline Tevent_data data() const;
 	private:
@@ -17,17 +17,17 @@ namespace state_machine {
 	}; // Event
 
 	template <class Tevent_id, class Tevent_data>
-	EventBase<Tevent_id, Tevent_data>::EventBase(const Tevent_id& id, const Tevent_data& data): m_id(id), m_data(data) {
+	StateMachineEvent<Tevent_id, Tevent_data>::StateMachineEvent(const Tevent_id& id, const Tevent_data& data): m_id(id), m_data(data) {
 
 	}
 
 	template <class Tevent_id, class Tevent_data>
-	inline Tevent_id EventBase<Tevent_id, Tevent_data>::id() const {
+	inline Tevent_id StateMachineEvent<Tevent_id, Tevent_data>::id() const {
 		return m_id;
 	}
 
 	template <class Tevent_id, class Tevent_data>
-	inline Tevent_data EventBase<Tevent_id, Tevent_data>::data() const {
+	inline Tevent_data StateMachineEvent<Tevent_id, Tevent_data>::data() const {
 		return m_data;
 	}
 

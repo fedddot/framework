6bd755160e6b731be94c4604e90d41882cb96500
idan.rom <idanrommm@gmail.com>
2023-12-30 11:41:23 +0200

    renamed

--- communication/transport_layer/src/uart.cpp
+++
@@ -1,7 +0,0 @@
-#include "uart.hpp"
-
-using namespace communication;
-
-Uart::Uart(BaudRate baud_rate, Parity parity, StopBits stop_bits, BitsNumber bits_number): m_baud_rate(baud_rate), m_parity(parity), m_stop_bits(stop_bits), m_bits_number(bits_number) {
-
-}
